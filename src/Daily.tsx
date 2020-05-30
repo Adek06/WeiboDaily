9238b201d289a6d16530fc36a175ea19064eb69d
Adek You <adek06@163.com>
2020-05-30 14:35:27 +0800

    feat(index): 使用 Bulma 重构页面 (#1)

--- src/Daily.tsx
+++ src/Daily.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-import { Card, Dropdown, DropdownButton, Image} from 'react-bootstrap'
-import { Row, Col } from 'react-bootstrap';
-import { HOST } from './Config';
+import './Daily.css'
+import {HOST} from './Config';
 
 interface DProps {
     content: string;
@@ -15,52 +14,79 @@ interface DProps {
 }
 
 interface DState {
-
+    isActiveDropdown: boolean;
 }
 
-class Daily extends React.Component<DProps, DState>{
+class Daily extends React.Component<DProps, DState> {
     constructor(props: DProps) {
         super(props)
         this.state = {
-
+            isActiveDropdown: false
         }
         console.log(this.props.iid)
         this.delDaily = this.delDaily.bind(this)
+        this.activeDropdown = this.activeDropdown.bind(this)
     }
 
     delDaily() {
         let that = this;
-        axios.delete(HOST+'/dailies/'+that.props.iid).then(function(e){
+        axios.delete(HOST + '/dailies/' + that.props.iid).then(function () {
             that.props.getDailies();
+        })
+    }
+
+    activeDropdown() {
+        let self = this;
+        let isActiveDropdown = self.state.isActiveDropdown
+        self.setState({
+            isActiveDropdown: !isActiveDropdown
         })
     }
 
     render() {
         let img = null;
         if (this.props.imgUrl !== "") {
-            img = <Image src={this.props.imgUrl} fluid />
+            img = <img alt={this.props.imgUrl} src={this.props.imgUrl}/>
         }
         return (
-            <Row>
-                <Col md={12}>
-                    <Card>
-                        <Card.Header>
-                            <Row>
-                                <Col md={11}>{this.props.createTime}</Col>
-                                <DropdownButton id="dropdown-basic-button" size="sm" variant="secondary" title="編輯">
-                                    <Dropdown.Item>編輯</Dropdown.Item>
-                                    <Dropdown.Item>分享</Dropdown.Item>
-                                    <Dropdown.Item onClick={this.delDaily}>刪除</Dropdown.Item>
-                                </DropdownButton>
-                            </Row>
-                        </Card.Header>
-                        <Card.Body>
-                            <div>{this.props.content}</div>
-                            {img}
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+            <div className="dailyBlock">
+                <div className="column is-12">
+                    <div className="level">
+                        <div className="level-left">
+                            <div className="level-item">
+                                {this.props.createTime}
+                            </div>
+                        </div>
+                        <div className="level-right">
+                            <div className="level-item">
+                                <div className={"dropdown " + (this.state.isActiveDropdown? "is-active": "") }  onClick={this.activeDropdown}>
+                                    <div className="dropdown-trigger">
+                                        <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                                            <span>编辑</span>
+                                            <span className="icon is-small">
+                                                <i className="fas fa-angle-down" aria-hidden="true" />
+                                            </span>
+                                        </button>
+                                    </div>
+                                    <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                        <div className="dropdown-content">
+                                            <a href="#" className="dropdown-item">
+                                                编辑
+                                            </a>
+                                            <hr className="dropdown-divider"/>
+                                            <a onClick={this.delDaily} className="dropdown-item">
+                                                删除
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>{this.props.content}</div>
+                    {img}
+                </div>
+            </div>
         )
     }
 }
